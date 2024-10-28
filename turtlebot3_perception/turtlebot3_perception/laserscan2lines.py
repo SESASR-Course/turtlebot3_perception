3aeab21048466c7394a5a4a206486e0d6ee60209
Marco Ambrosio <marco.ambrosio@polito.it>
2024-10-28 10:05:51 +0000

    manage vertical lines

--- turtlebot3_perception/turtlebot3_perception/laserscan2lines.py
+++ turtlebot3_perception/turtlebot3_perception/laserscan2lines.py
@@ -27,9 +27,11 @@ def timeit(func):
 def laserscan2lines(scan: LaserScan):
     """
     A function to detect lines from a LaserScan message using RANSAC.
+    A normal line is represented by y = mx + c, where m is the slope and c is the y-intercept.
+    If the line is vertical (angle >85°), the line equation is x = c, where c is the x-intercept.
 
     :param scan: A sensor_msgs/LaserScan message
-    :return: A list of detected lines, where each line is a list of points (x, y) and the line equation (slope, intercept)
+    :return: A list of detected lines, where each line is a list of points (x, y) and the line equation (slope, intercept). If the line is vertical, the slope is infinity.
     """
 
     # Convert LaserScan ranges to Cartesian coordinates
@@ -53,25 +55,37 @@ def laserscan2lines(scan: LaserScan):
     min_points_for_line = 30  # Minimum points to define a line
     ransac = RANSACRegressor(residual_threshold=0.1)
 
-    while len(points) > min_points_for_line:
-        # Fit RANSAC model to the remaining points
-        ransac.fit(points[:, 0].reshape(-1, 1), points[:, 1])
+    while len(points) > min_points_for_line: 
+        with np.errstate(divide='ignore'):
+            stDevX = np.std(points[:, 0]) 
+            stDevY = np.std(points[:, 1])
+            verticality = stDevY/stDevX
+
+        if verticality < 15:
+            # Fit RANSAC model to the remaining points
+            ransac.fit(points[:, 0].reshape(-1, 1), points[:, 1])
+            line_model = ransac.estimator_
+            slope = line_model.coef_[0]   # m in y = mx + c
+            intercept = line_model.intercept_  # c in y = mx + c
+        else:
+            ransac.fit(points[:, 1].reshape(-1, 1), points[:, 0])
+            line_model = ransac.estimator_
+            slope = np.inf
+            intercept = line_model.intercept_  # c in x = c
 
         # Extract inliers and outliers
         inlier_mask = ransac.inlier_mask_
         line_points = points[inlier_mask]
-        line_model = ransac.estimator_
 
         # Save the detected line
         if len(line_points) >= min_points_for_line:
             lines.append(line_points)
 
             # Get line equation parameters
-            slope = line_model.coef_[0]   # m in y = mx + c
-            intercept = line_model.intercept_  # c in y = mx + c
             line_equations.append((slope, intercept))
         # Remove inlier points and continue
         points = points[~inlier_mask]
+
     
     return lines, line_equations
 
@@ -112,9 +126,16 @@ class LaserScan2Lines(Node):
 
             # Plot the fitted line
             slope, intercept = line_equations[idx]
-            x_vals = np.linspace(np.min(line_points[:, 0]), np.max(line_points[:, 0]), 100)
-            y_vals = slope * x_vals + intercept
-            plt.plot(x_vals, y_vals, label=f'Line {idx+1}: y = {slope:.2f}x + {intercept:.2f}', linewidth=2)
+            if slope == np.inf:
+                # vertical lines (x = c)
+                x_vals = np.full(100, intercept)
+                y_vals = np.linspace(np.min(line_points[:, 1]), np.max(line_points[:, 1]), 100)
+                plt.plot(x_vals, y_vals, label=f'Line {idx+1}: x = {intercept:.2f}', linewidth=2)
+            else:
+                # normal lines (y = mx + c)
+                x_vals = np.linspace(np.min(line_points[:, 0]), np.max(line_points[:, 0]), 100)
+                y_vals = slope * x_vals + intercept
+                plt.plot(x_vals, y_vals, label=f'Line {idx+1}: y = {slope:.2f}x + {intercept:.2f}', linewidth=2)
         plt.xlim(-3.5, 3.5)
         plt.ylim(-3.5, 3.5)
         plt.axis('equal')
